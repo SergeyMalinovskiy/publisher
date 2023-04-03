4c783e9eda1566de572fa429d878ef305c10b1a6
sergey.malinovskiy <sergey.malinovskiy@immelman.ru>
2023-04-03 14:49:38 +0300

    Init project from template

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER publisher SUPERUSER;
+CREATE DATABASE publisher OWNER publisher ENCODING 'utf-8';
